f2a5d4430278a2089a77f76fdee605fd02b88d2e
Mahmoud Salah <ms685116@gmail.com>
2024-01-02 20:38:03 +0200

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""get data from jsonplaceholder api"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    tasks = requests.get(todos_url, params={'userId': sys.argv[1]}).json()
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(f"{users_url}/{sys.argv[1]}").json()
+    complete_tasks = [
+        task.get("title") for task in tasks if task.get("completed")
+    ]
+    name = user.get("name")
+    done = f'{len(complete_tasks)}/{len(tasks)}'
+    print(
+        f"Employee {name} is done with tasks({done}):"
+    )
+    for task in complete_tasks:
+        print(f'\t {task}')
+
+    with open(f'{user.get("id")}.csv', 'w') as f:
+      writer = csv.writer(f)
+      for task in tasks:
+          writer.writerow([
+            task.get('userId'),
+            name,
+            task.get('completed'),
+            task.get('title'),
+          ])
