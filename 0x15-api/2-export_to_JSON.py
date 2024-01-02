e3b4243df8fc427a30fd83387bf0fb255f2d587a
Mahmoud Salah <ms685116@gmail.com>
2024-01-02 21:16:06 +0200

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""get data from jsonplaceholder api"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    tasks = requests.get(todos_url, params={"userId": sys.argv[1]}).json()
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(f"{users_url}/{sys.argv[1]}").json()
+    complete_tasks = [
+        task.get("title") for task in tasks if task.get("completed")
+    ]
+    name = user.get("name")
+    done = f"{len(complete_tasks)}/{len(tasks)}"
+    print(f"Employee {name} is done with tasks({done}):")
+    for task in complete_tasks:
+        print(f"\t {task}")
+
+    dict = {
+        user.get("id"): [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username"),
+            }
+            for task in tasks
+        ]
+    }
+    with open(f"{user.get('id')}.json", "w") as f:
+        json.dump(dict, f)
