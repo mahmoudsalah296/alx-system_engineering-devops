ef7695802ba640909f08ee8f2bf886c951426907
Mahmoud Salah <ms685116@gmail.com>
2024-01-02 20:16:43 +0200

    get user todos list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,15 @@ import sys
 
 
 if __name__ == "__main__":
-    tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/todos",
-        params={'userId': sys.argv[1]}
-    )
-    user = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    )
-    json_tasks = tasks.json()
-    tasks_number = len(json_tasks)
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    tasks = requests.get(todos_url, params={'userId': sys.argv[1]}).json()
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(f"{users_url}/{sys.argv[1]}").json()
     complete_tasks = [
-        task.get("title") for task in json_tasks if task.get("completed")
+        task.get("title") for task in tasks if task.get("completed")
     ]
-    tasks_completed_num = len(complete_tasks)
-    name = user.json().get("name")
-    done = f'{tasks_completed_num}/{tasks_number}'
+    name = user.get("name")
+    done = f'{len(complete_tasks)}/{len(tasks)}'
     print(
         f"Employee {name} is done with tasks({done}):"
     )
