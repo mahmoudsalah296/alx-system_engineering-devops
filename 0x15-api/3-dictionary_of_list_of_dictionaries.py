2aca9650456238d46e73cab0c4523ec4b3d38164
Mahmoud Salah <ms685116@gmail.com>
2024-01-02 21:56:56 +0200

    list of all employees todos

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,11 +8,19 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as file:
-        json.dump({
-            user.get("id"): [{
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": user.get("username")
-            } for task in requests.get(url + "todos",
-                          params={"userId": user.get("id")}).json()]
-            for user in users}, file)
+        json.dump(
+            {
+                user.get("id"): [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": user.get("username"),
+                    }
+                    for task in requests.get(
+                        url + "todos", params={"userId": user.get("id")}
+                    ).json()
+                ]
+                for user in users
+            },
+            file,
+        )
